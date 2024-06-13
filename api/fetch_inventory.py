766d2bdee40acc6b48a9b94c2ed18b938ad8cbd0
Philippe Vallerand <pvallerand1@gmail.com>
2024-06-13 15:19:18 -0400

    ajout page création d'item

--- api/fetch_inventory.py
+++ api/fetch_inventory.py
@@ -1,4 +1,4 @@
-import urllib3, json
+import urllib3, json, base64
 
 
 
@@ -6,3 +6,22 @@ def all_inventory_items():
     response = urllib3.request("GET", "http://localhost:8081/api/v1/inventory/getAll")
     data = json.loads(response.data)
     return data
+
+def add_item_to_inventory(form_json, data):
+    encoded_body = json.dumps({
+        "name": form_json["name"],
+        "description": form_json["description"],
+        "category": form_json["category"],
+        "image": {
+            "data": base64.b64encode(bytes(bytearray(data))).decode("utf-8"),
+        },
+        "quantity": form_json["quantity"],
+        "location": form_json["location"]
+    })
+    response = urllib3.request("POST", "http://localhost:8081/api/v1/inventory/create",
+                               headers={'Content-Type': 'application/json'},
+                               body=encoded_body
+                            )
+    
+    data = json.loads(response.data)
+    return data["id"]
--- flaskApp.py
+++ flaskApp.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from api import fetch_inventory
 app = Flask(__name__)
+import sys
 
 
 @app.route('/')
@@ -11,3 +12,15 @@ def home_page():
 def items_list():
     items_list = fetch_inventory.all_inventory_items()
     return render_template('listItems/items_list.html', items_list=items_list)
+
+@app.route("/createitem")
+def create_item():
+    return render_template('createItem/create_item.html')
+
+@app.route("/posttest", methods=['POST'])
+def post_test():
+    file_data = request.files["image"].read()
+    form_data = request.form
+    item_id = fetch_inventory.add_item_to_inventory(form_data, file_data)
+    print(item_id, file=sys.stderr)
+    return render_template('createItem/create_item.html')
